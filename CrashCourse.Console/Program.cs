9fe3b62fe0119c977905b35c4a315eee18681f98
njezichigozie <njezichigozie@yahoo.com>
2023-10-06 09:27:42 +0100

    Algorithm based

---
+++ CrashCourse.Console/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var inputs = new int[]
+{
+    1, 1, 2,2,2,3,3,3,3,4,4,4,4,4,5,5,5,5,5
+};
+
+if (!inputs.Any())
+{
+    Console.WriteLine("Nothing was passed. Please enter values");
+    return;
+}
+
+int maxOccurrence = 0;
+int maxItem = 0;
+
+Dictionary<int, int> frequencyMap = new Dictionary<int, int>();
+foreach (var item in inputs)
+{
+    if (frequencyMap.ContainsKey(item))
+    {
+        frequencyMap[item]++;
+    }
+    else
+    {
+        frequencyMap[item] = 1;
+    }
+    if (frequencyMap[item] > maxOccurrence)
+    {
+        maxOccurrence = frequencyMap[item];
+        maxItem = item;
+    }
+}
+Console.WriteLine($"The highest occurring item is {maxItem}, with a frequency of {maxOccurrence}");
+Console.ReadLine();
